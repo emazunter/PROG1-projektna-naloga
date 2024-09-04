8742b651927f098e4732e47adb78ad44f9251672
Ema Žunter <zunterema@gmail.com>
2024-09-04 11:21:13 +0200

    zakaj mi git updati ne delajo

--- definicije/avtomat.ml
+++ definicije/avtomat.ml
@@ -6,7 +6,7 @@ type t = {
   sklad_1: sklad;
   sklad_2: sklad;
   sklad_3: sklad;
-  prehodi : ()
+  prehodi : (stanje * char * char * char * stanje * char list * char list * char list) list
   zacetno_stanje : stanje;
   zacetni_sklad1: sklad;
   zacetni_sklad2: sklad;
@@ -38,17 +38,17 @@ let dodaj_sprejemno_stanje stanje avtomat =
     sprejemna_stanja = stanje :: avtomat.sprejemna_stanja;
   }
 
-(* let dodaj_prehod stanje1 znak znak_na_s1 znak_na_s2 znak_na_s3 stanje2 avtomat =
-  { avtomat with prehodi = (stanje1, znak, stanje2) :: avtomat.prehodi } *)
+let dodaj_prehod stanje1 znak znak_na_s1 znak_na_s2 znak_na_s3 stanje2 avtomat =
+  { avtomat with prehodi = (stanje1, znak, stanje2) :: avtomat.prehodi }
 
-let prehodna_funkcija avtomat stanje znak =
+(* let prehodna_funkcija avtomat stanje znak =
   match
     List.find_opt
       (fun (stanje1, znak', _stanje2) -> stanje1 = stanje && znak = znak')
       avtomat.prehodi
   with
   | None -> None
-  | Some (_, _, stanje2) -> Some stanje2
+  | Some (_, _, stanje2) -> Some stanje2 *)
 
 let zacetno_stanje avtomat = avtomat.zacetno_stanje
 let seznam_stanj avtomat = avtomat.stanja
@@ -57,7 +57,7 @@ let seznam_prehodov avtomat = avtomat.prehodi
 let je_sprejemno_stanje avtomat stanje =
   List.mem stanje avtomat.sprejemna_stanja
 
-let enke_1mod3 =
+(* let enke_1mod3 =
   let q0 = Stanje.iz_niza "q0"
   and q1 = Stanje.iz_niza "q1"
   and q2 = Stanje.iz_niza "q2" in
@@ -70,4 +70,4 @@ let preberi_niz avtomat q niz =
   let aux acc znak =
     match acc with None -> None | Some q -> prehodna_funkcija avtomat q znak
   in
-  niz |> String.to_seq |> Seq.fold_left aux (Some q)
+  niz |> String.to_seq |> Seq.fold_left aux (Some q) *)
