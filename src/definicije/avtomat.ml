6b9d6b33c0fd7c46b1953223fb3ac07a78b2e132
Ema Žunter <zunterema@gmail.com>
2024-10-23 22:27:31 +0200

    naredila avtomat (???)

--- src/definicije/avtomat.ml
+++ src/definicije/avtomat.ml
@@ -10,7 +10,7 @@ type t = {
   sprejemna_stanja : stanje list;
 }
 
-let prazen_avtomat zacetno_stanje =
+let prazen_avtomat zacetno_stanje s =
   {
     stanja = [ zacetno_stanje ];
     sklad = s;
@@ -51,11 +51,11 @@ let seznam_prehodov avtomat = avtomat.prehodi
 let je_sprejemno_stanje avtomat stanje =
   List.mem stanje avtomat.sprejemna_stanja
 
-let preberi_niz avtomat q niz =
+(* let preberi_niz avtomat q niz =
   let aux acc znak =
     match acc with None -> None | Some q -> prehodna_funkcija avtomat q znak
   in
-  niz |> String.to_seq |> Seq.fold_left aux (Some q)
+  niz |> String.to_seq |> Seq.fold_left aux (Some q) *)
 
 let sintaksa_oklepajev = 
   let q0 = Stanje.iz_niza "q0"
@@ -68,89 +68,72 @@ let sintaksa_oklepajev =
   |> dodaj_nesprejemno_stanje q1
   |> dodaj_nesprejemno_stanje q2 
   |> dodaj_nesprejemno_stanje q3
-  |> dodaj_nesprejemno_stanje q4
-  |> dodaj_nesprejemno_stanje q5
-  |> dodaj_nesprejemno_stanje q6
+  |> dodaj_sprejemno_stanje q4
+  |> dodaj_sprejemno_stanje q5
+  |> dodaj_sprejemno_stanje q6
   |> dodaj_nesprejemno_stanje q7
+
   |> dodaj_prehod q0 '(' "" q1 ["1"]
   |> dodaj_prehod q0 '[' "" q2 ["2"]
   |> dodaj_prehod q0 '{' "" q3 ["3"]
+
   |> dodaj_prehod q1 '(' "1" q1 ["1"; "1"]
   |> dodaj_prehod q1 '[' "1" q2 ["2"; "1"]
   |> dodaj_prehod q1 '{' "1" q3 ["3"; "1"]
   |> dodaj_prehod q1 ')' "1" q4 []
   |> dodaj_prehod q1 ']' "1" q7 ["ne"]
   |> dodaj_prehod q1 '}' "1" q7 ["ne"]
+
   |> dodaj_prehod q2 '(' "2" q1 ["1"; "2"]
   |> dodaj_prehod q2 '[' "2" q2 ["2"; "2"]
   |> dodaj_prehod q2 '{' "2" q3 ["3"; "2"]
   |> dodaj_prehod q2 ')' "2" q7 ["ne"]
   |> dodaj_prehod q2 ']' "2" q5 []
   |> dodaj_prehod q2 '}' "2" q7 ["ne"]
+
   |> dodaj_prehod q3 '(' "3" q1 ["1"; "3"]
   |> dodaj_prehod q3 '[' "3" q2 ["2"; "3"]
   |> dodaj_prehod q3 '{' "3" q3 ["3"; "3"]
   |> dodaj_prehod q3 ')' "3" q7 ["ne"]
   |> dodaj_prehod q3 ']' "3" q7 ["ne"]
   |> dodaj_prehod q3 '}' "3" q6 []
-  |> dodaj_prehod q4 '(' x q1 ["1"]
+  
+  |> dodaj_prehod q4 '(' vrh q1 ["1"; vrh]
+  |> dodaj_prehod q4 '[' vrh q2 ["2"; vrh]
+  |> dodaj_prehod q4 '{' vrh q3 ["3"; vrh]
+  |> dodaj_prehod q4 ')' "1" q4 []
+  |> dodaj_prehod q4 ']' "2" q5 []
+  |> dodaj_prehod q4 '(' "3" q6 []
+  |> dodaj_prehod q4 ')' "2" q7 ["ne"]
+  |> dodaj_prehod q4 ']' "1" q7 ["ne"]
+  |> dodaj_prehod q4 '(' "2" q7 ["ne"]
+  |> dodaj_prehod q4 ')' "3" q7 ["ne"]
+  |> dodaj_prehod q4 ']' "3" q7 ["ne"]
+  |> dodaj_prehod q4 '(' "1" q7 ["ne"]
 
+  |> dodaj_prehod q5 '(' vrh q1 ["1"; vrh]
+  |> dodaj_prehod q5 '[' vrh q2 ["2"; vrh]
+  |> dodaj_prehod q5 '{' vrh q3 ["3"; vrh]
+  |> dodaj_prehod q5 ')' "1" q4 []
+  |> dodaj_prehod q5 ']' "2" q5 []
+  |> dodaj_prehod q5 '(' "3" q6 []
+  |> dodaj_prehod q5 ')' "2" q7 ["ne"]
+  |> dodaj_prehod q5 ']' "1" q7 ["ne"]
+  |> dodaj_prehod q5 '(' "2" q7 ["ne"]
+  |> dodaj_prehod q5 ')' "3" q7 ["ne"]
+  |> dodaj_prehod q5 ']' "3" q7 ["ne"]
+  |> dodaj_prehod q5 '(' "1" q7 ["ne"]
 
-
-
-
-
-(* let zamenjaj = function
-| "+" | "-" | "x" | "/" -> "o"
-| "1" | "2" | "3" | "4" | "5" | "6" | "7" |"8" | "9" |"0" -> "n"
-| "(" -> "u"
-| ")" -> "z"
-| x -> x
-
-let dodaj_prehod_moj_avtomat stanje1 prebrano vrh stanje2 nov_vrh avtomat =
-  let novo = zamenjaj prebrano in
-   dodaj_prehod stanje1 novo vrh stanje2 nov_vrh avtomat
-
-let preverjanje_pravilnosti =
-  let zacetno = Stanje.iz_niza "zacetno"
-  and stevilo = Stanje.iz_niza "stevilo"
-  and operacija = Stanje.iz_niza "operacija"
-  and uklepaj = Stanje.iz_niza "uklepaj"
-  and zaklepaj = Stanje.iz_niza "zaklepaj" 
-  and nesprejemno = Stanje.iz_niza "nesprejemno" in
-  prazen_avtomat zacetno (Sklad.ustvari 'p') 
-  |> dodaj_sprejemno_stanje zacetno
-  |> dodaj_sprejemno_stanje zaklepaj
-  |> dodaj_sprejemno_stanje stevilo
-  |> dodaj_nesprejemno_stanje operacija
-  |> dodaj_nesprejemno_stanje uklepaj
-  |> dodaj_nesprejemno_stanje nesprejemno 
-  |> dodaj_prehod_moj_avtomat zacetno p sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat zacetno o sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat zacetno n sklad stevilo sklad
-  |> dodaj_prehod_moj_avtomat zacetno u sklad uklepaj sklad
-  |> dodaj_prehod_moj_avtomat zacetno z sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat operacija o sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat operacija n sklad stevilo sklad
-  |> dodaj_prehod_moj_avtomat operacija u sklad uklepaj (Sklad.dodaj "1" sklad)
-  |> dodaj_prehod_moj_avtomat operacija z sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat stevilo o sklad operacija sklad
-  |> dodaj_prehod_moj_avtomat stevilo n sklad stevilo sklad
-  |> dodaj_prehod_moj_avtomat stevilo u sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat stevilo z ("1" :: sklad) zaklepaj sklad
-  |> dodaj_prehod_moj_avtomat stevilo z ("p" :: sklad) nesprejemno (Sklad.dodaj "p" sklad)
-  |> dodaj_prehod_moj_avtomat uklepaj o sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat uklepaj n sklad uklepaj ("1" :: sklad)
-  |> dodaj_prehod_moj_avtomat uklepaj u sklad uklepaj ("1" :: sklad)
-  |> dodaj_prehod_moj_avtomat uklepaj z sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat zaklepaj o ("1" :: sklad) operacija sklad
-  |> dodaj_prehod_moj_avtomat zaklepaj o ("p" :: sklad) nesprejemno (Sklad.dodaj "p" sklad)
-  |> dodaj_prehod_moj_avtomat zaklepaj n sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat zaklepaj u sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat zaklepaj z ("1" :: sklad) zaklepaj sklad
-  |> dodaj_prehod_moj_avtomat zaklepaj z ("p" :: sklad) nesprejemno (Sklad.dodaj "p" sklad)
-  |> dodaj_prehod_moj_avtomat nesprejemno o sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat nesprejemno n sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat nesprejemno u sklad nesprejemno sklad
-  |> dodaj_prehod_moj_avtomat nesprejemno "z" sklad nesprejemno sklad *)
+  |> dodaj_prehod q6 '(' vrh q1 ["1"; vrh]
+  |> dodaj_prehod q6 '[' vrh q2 ["2"; vrh]
+  |> dodaj_prehod q6 '{' vrh q3 ["3"; vrh]
+  |> dodaj_prehod q6 ')' "1" q4 []
+  |> dodaj_prehod q6 ']' "2" q5 []
+  |> dodaj_prehod q6 '(' "3" q6 []
+  |> dodaj_prehod q6 ')' "2" q7 ["ne"]
+  |> dodaj_prehod q6 ']' "1" q7 ["ne"]
+  |> dodaj_prehod q6 '(' "2" q7 ["ne"]
+  |> dodaj_prehod q6 ')' "3" q7 ["ne"]
+  |> dodaj_prehod q6 ']' "3" q7 ["ne"]
+  |> dodaj_prehod q6 '(' "1" q7 ["ne"]
 
