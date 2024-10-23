0ab6a9556fd8f1a8c491315de20976f2d4cedc24
Ema Žunter <zunterema@gmail.com>
2024-10-23 22:44:38 +0200

    znižala ambicije in šaltala na tekstovni vmesnik

--- src/definicije/zagnaniAvtomat.ml
+++ src/definicije/zagnaniAvtomat.ml
@@ -1,23 +1,24 @@
-type t = { avtomat : Avtomat.t; trak : Trak.t; stanje : Stanje.t }
+type t = { avtomat : Avtomat.t; trak : Trak.t; stanje : Stanje.t sklad: Sklad.t}
 
 let pozeni avtomat trak =
-  { avtomat; trak; stanje = Avtomat.zacetno_stanje avtomat }
+  { avtomat; trak; stanje = Avtomat.zacetno_stanje avtomat sklad = Avtomat.zacetni_sklad avtomat }
 
 let avtomat { avtomat; _ } = avtomat
 let trak { trak; _ } = trak
 let stanje { stanje; _ } = stanje
 
-let korak_naprej { avtomat; trak; stanje } =
+let korak_naprej { avtomat; trak; stanje; sklad } =
   if Trak.je_na_koncu trak then None
   else
     let stanje' =
-      Avtomat.prehodna_funkcija avtomat stanje (Trak.trenutni_znak trak)
-      (* uredi to s skladom? *)
+      Avtomat.prehodna_funkcija avtomat stanje (Sklad.vrh sklad) (Trak.trenutni_znak trak)
+
     in
     match stanje' with
     | None -> None
+    (* to se mores
     | Some stanje' ->
-        Some { avtomat; trak = Trak.premakni_naprej trak; stanje = stanje' }
+        Some { avtomat; trak = Trak.premakni_naprej trak; stanje = stanje' } *)
 
 let je_v_sprejemnem_stanju { avtomat; stanje; _ } =
   Avtomat.je_sprejemno_stanje avtomat stanje
